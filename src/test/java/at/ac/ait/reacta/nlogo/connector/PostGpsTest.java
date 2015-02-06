ee84513e5abdd5eb534f326df41c32060b74e394
Christoph Ruggenthaler <christoph.ruggenthaler@gmail.com>
2015-02-06 13:20:17 +0100

    ignored test

--- src/test/java/at/ac/ait/reacta/nlogo/connector/PostGpsTest.java
+++ src/test/java/at/ac/ait/reacta/nlogo/connector/PostGpsTest.java
@@ -1,32 +1,29 @@
 package at.ac.ait.reacta.nlogo.connector;
 
-import java.io.IOException;
-
-import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class PostGpsTest {
 
 	@Test
+	@Ignore
 	public void httpTest() {
 
 		String arfId = "aha";
 		double lng = 1.2;
 		double lat = 3.4;
 
-		DefaultHttpClient client = new DefaultHttpClient();
-
 		try {
-			HttpGet post = new HttpGet(
-					"http://localhost:12003/log/get?arfId=1232&lat=1.2");
-			HttpResponse resp = client.execute(post);
-
+			DefaultHttpClient client = new DefaultHttpClient();
+			HttpGet post = new HttpGet("http://localhost:12003/log/get?arfId="
+					+ arfId + "&lat=" + lat + "&lng=" + lng);
+			client.execute(post);
 			// Consume and close Connection
 			post.releaseConnection();
 
-		} catch (IOException e) {
+		} catch (Exception e) {
 
 			e.printStackTrace();
 		}
